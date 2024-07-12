a7c147937d14f184c77db1a014f2def927935025
imaditya05 <imaditya05@gmail.com>
2024-07-12 15:10:42 +0530

    Added Message-Card

---
+++ src/components/MessageCard.tsx
@@ -0,0 +1,24 @@
+"use client";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+const MessageCard = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent></CardContent>
+      <CardFooter></CardFooter>
+    </Card>
+  );
+};
+
+export default MessageCard;
