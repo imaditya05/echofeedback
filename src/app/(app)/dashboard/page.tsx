3c1a3d31c034d0085c533625a6b03ae69d182957
imaditya05 <imaditya05@gmail.com>
2024-07-14 21:19:54 +0530

    Added logic

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+"use client";
+import { useToast } from "@/components/ui/use-toast";
+import { Message } from "@/model/User";
+import { acceptMessageValidation } from "@/schema/acceptMessageSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+
 
 const page = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [loading, setIsLoading] = useState(false);
+  const [isSwitchLoading, setIsSwitchLoading] = useState(false);
 
-export default page
+  const { toast } = useToast();
+
+  const handleMessageDelete = (messageId: string) => {
+    setMessages(messages.filter((message) => message._id !== messageId));
+  };
+
+  const { data: session } = useSession();
+
+  const form = useForm({
+    resolver: zodResolver(acceptMessageValidation)
+  })
+
+  return <div>Dashboard</div>;
+};
+
+export default page;
--- src/schema/acceptMessageSchema.ts
+++ src/schema/acceptMessageSchema.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
 
-const acceptMessageValidation = z.object({
+export const acceptMessageValidation = z.object({
   acceptMessages: z.boolean(),
 });
