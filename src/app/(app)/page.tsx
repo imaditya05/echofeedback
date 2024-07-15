6309c4e2c176114d64dae2c6e8d79549f5e3be68
imaditya05 <imaditya05@gmail.com>
2024-07-15 22:32:36 +0530

    Completed home page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,6 +1,61 @@
+"use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
+import messages from "@/messages.json";
+import Autoplay from "embla-carousel-autoplay";
+import { Mail } from "lucide-react";
 
 export default function Home() {
   return (
-    <div>EchoFeedback</div>
+    <div className="h-screen">
+      <main className="flex-grow flex flex-col items-center justify-center px-4 md:px-24 py-12">
+        <section className="text-center mb-8 md:mb-12">
+          <h1 className="text-3xl md:text-5xl font-bold">
+            Collect Honest, Anonymous Feedback from Your Customers
+          </h1>
+          <p className="mt-3 md:mt-4 text-base md:text-lg">
+            Easily create feedback links, share them with your audience, and
+            gain the insights you need to grow and succeed.
+          </p>
+        </section>
+        <Carousel
+          plugins={[Autoplay({ delay: 2000 })]}
+          className="w-full max-w-lg md:max-w-xl"
+        >
+          <CarouselContent>
+            {messages.map((message, index) => (
+              <CarouselItem key={index} className="p-4">
+                <Card>
+                  <CardHeader>
+                    <CardTitle>{message.title}</CardTitle>
+                  </CardHeader>
+                  <CardContent className="flex flex-col md:flex-row items-start space-y-2 md:space-y-0 md:space-x-4">
+                    <Mail className="flex-shrink-0" />
+                    <div>
+                      <p>{message.content}</p>
+                      <p className="text-xs text-muted-foreground">
+                        {message.received}
+                      </p>
+                    </div>
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious />
+          <CarouselNext />
+        </Carousel>
+      </main>
+      <footer className="text-center p-4 md:p-6 bg-gray-900 text-white">
+        © 2023 True Feedback. All rights reserved.
+      </footer>
+    </div>
   );
 }
